096442378a91cb840d49dfc6dec6929945310991
h0pk1do <h0pk1do@tutanota.com>
2022-03-08 16:22:10 -0600

    Discount
    Added the scanner input for age and outcome for ticket price after discounts applied

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class BoardingPass {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Scanner scanner = new Scanner(System.in);
+        int price = 100;
+        int age = scanner.nextInt();
+        if (age <= 12) {
+            System.out.println("Price: " + price / .5);
+        } else if (age >= 60)  {
+            System.out.println("Price: " + price / .6);
+        } else if (Gender == Female) {
+            System.out.println("Price: " + price / .25);
+        }
+
+
     }
+
 }
